3e4d5c6cc65325ae2267ba69bb5f90da17224f9a
Lillian Rose <me@lillianro.se>
2024-09-01 10:40:05 -0400

    chore: Initial commit

---
+++ modules/dots/fish/config.fish
@@ -0,0 +1,13 @@
+function fish_greeting
+   hyfetch
+end
+
+alias env="echo BAD GIRL"
+
+alias cat=bat
+alias ls="exa -lha"
+alias grep=rg
+
+fish_add_path /usr/local/bin
+fish_add_path ~/.local/bin
+fish_add_path ~/.cargo/bin
---
+++ modules/lily/keys/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+gpg --import 985A88CE54A3CB82-me@lillianrose.asc.pub
+
+echo "Running ssh-keygen -K"
+ssh-keygen -K
+
+mkdir -p ~/.ssh
+sudo mv id_ed25519_sk_rk ~/.ssh/id_ed25519_sk
+sudo mv id_ed25519_sk_rk.pub ~/.ssh/id_ed25519_sk.pub
+sudo chmod 700 ~/.ssh
+sudo chmod 600 ~/.ssh/id_ed25519_sk
+sudo chmod 644 ~/.ssh/id_ed25519_sk.pub
+sudo chown -R $USER: ~/.ssh
