8d3a708cbdf60e1574801d248099366144841444
Lillian Rose <me@lillianro.se>
2024-09-09 07:20:06 -0400

    fix: set user perms in virt module

--- modules/virt/enable-services.sh
+++ modules/virt/enable-services.sh
@@ -4,3 +4,8 @@ for drv in qemu interface network nodedev nwfilter secret storage; do
     sudo systemctl enable virt${drv}d.service;
     sudo systemctl enable virt${drv}d{,-ro,-admin}.socket;
 done
+
+sudo usermod -aG libvirt $USER
+sudo setfacl -R -b /var/lib/libvirt/images/
+sudo setfacl -R -m u:${USER}:rwX /var/lib/libvirt/images/
+sudo setfacl -m d:u:${USER}:rwx /var/lib/libvirt/images/
